90ccc4da2dffe4b1afd9f565adc3786dc273a40c
wsx2792 <wsx2792@gmail.com>
2021-06-15 23:31:18 +0900

    typeorm 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     UsersModule,
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": "localhost",
+      "port": 5432,
+      "username": "davidchae",
+      "password": "Djaakdi1",
+      "database": "postgres",
+      "synchronize": true,
+      "logging": true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
