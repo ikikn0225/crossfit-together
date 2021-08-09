9154e3ecafad5cee1518938c8ce9b2f961d34855
HeadCastle <wsx2792@gmail.com>
2021-08-09 14:19:48 +0900

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -41,9 +41,8 @@ import { UploadsModule } from './uploads/uploads.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'production',
       validationSchema: Joi.object({
-        NODE_ENV:             Joi.string().valid('dev', 'production', 'test').required(),
+        NODE_ENV:             Joi.string().valid('dev', 'prod').required(),
         DB_HOST:              Joi.string().required(),
         DB_PORT:              Joi.string().required(),
         DB_USERNAME:          Joi.string().required(),
